498acd0405827023c01a6cf9750e8ef80f4ea89e
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-07-03 15:30:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 from shapely.geometry import mapping
 
 # Função para carregar GeoJSON com cache seletivo
-@st.cache(suppress_st_warning=True)
+@st.cache_data  # Atualizado para usar st.cache_data
 def load_geojson(file_path):
     try:
         gdf = gpd.read_file(file_path)
@@ -28,22 +28,12 @@ def format_area(area):
 # Função para gerar GeoJSON dos polígonos filtrados
 def download_geojson(filtered_gdf):
     selected_features = []
-    for idx, row in filtered_gdf.iterrows():
+    for _, row in filtered_gdf.iterrows():
         geom = row['geometry']
         feature = {
             'type': 'Feature',
             'geometry': mapping(geom),
-            'properties': {
-                'nome_pa': row.get('nome_pa', 'N/A'),
-                'area_incra': row.get('area_incra', 'N/A'),
-                'area_polig': row.get('area_polig', 'N/A'),
-                'lotes': row.get('lotes', 'N/A'),
-                'quant_fami': row.get('quant_fami', 'N/A'),
-                'fase': row.get('fase', 'N/A'),
-                'data_criac': row.get('data_criac', 'N/A'),
-                'forma_obte': row.get('forma_obte', 'N/A'),
-                'data_obten': row.get('data_obten', 'N/A')
-            }
+            'properties': {col: row.get(col, 'N/A') for col in filtered_gdf.columns if col != 'geometry'}
         }
         selected_features.append(feature)
 
@@ -53,6 +43,21 @@ def download_geojson(filtered_gdf):
     }
 
     return json.dumps(feature_collection)
+
+# Função para obter o centro do estado ou município
+def get_center(gdf, state=None, municipality=None):
+    if municipality:
+        subset = gdf[gdf['municipio'] == municipality]
+    elif state:
+        subset = gdf[gdf['uf'] == state]
+    else:
+        return [-15.77972, -47.92972]  # Centro do Brasil (Brasília)
+    
+    if not subset.empty:
+        center = subset.geometry.unary_union.centroid
+        return [center.y, center.x]
+    else:
+        return [-15.77972, -47.92972]  # Centro do Brasil (Brasília)
 
 # Caminho para o arquivo GeoJSON
 geojson_path = "pasbr_geo.geojson"
@@ -65,8 +70,6 @@ if gdf is not None:
     st.title("Mapa interativo com os projetos de assentamento de reforma agrária no Brasil")
     st.markdown("(As informações exibidas neste site são públicas e estão disponíveis no [Portal de Dados Abertos](https://dados.gov.br/dados/conjuntos-dados/sistema-de-informacoes-de-projetos-de-reforma-agraria---sipra))")
     st.write("Contato: 6dsvj@pm.me")
-
-    m = folium.Map(location=[-24.0, -51.0], zoom_start=7)
 
     filters = {}
 
@@ -88,33 +91,35 @@ if gdf is not None:
     }
 
     options_lotes = [10, 50, 100, 300, 500, 800, 1200, 2000, 5000, 10000, 15000, 20000]
-    options_familias = options_lotes
     options_area_incra = [500, 1000, 5000, 10000, 30000, 50000, 100000, 200000, 400000, 600000]
 
-    selected_state = 'PARANÁ'
+    # Filtro de estado
+    state_options = [''] + sorted(gdf['uf'].dropna().unique().tolist())
+    selected_state = st.sidebar.selectbox("Escolha um estado:", state_options, index=0)
+    filters['uf'] = selected_state
 
-    # Filtrar municípios com base no estado selecionado
+    # Filtro de município
     if selected_state:
-        filtered_gdf_state = gdf[gdf['uf'] == selected_state]
-        municipality_options = [''] + sorted(filtered_gdf_state['municipio'].dropna().unique().tolist())
+        municipality_options = [''] + sorted(gdf[gdf['uf'] == selected_state]['municipio'].dropna().unique().tolist())
     else:
         municipality_options = [''] + sorted(gdf['municipio'].dropna().unique().tolist())
+    selected_municipality = st.sidebar.selectbox("Escolha um município:", municipality_options, index=0)
+    filters['municipio'] = selected_municipality
 
     for col, display_name in filter_columns.items():
         if col in gdf.columns or col in ['area_incra_min', 'area_polig_min']:
-            if col == 'municipio':
-                filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", municipality_options, format_func=lambda x: 'Nenhum' if x == "" else str(x))
-            elif col == 'lotes' or col == 'quant_fami':
-                options = [None] + sorted(options_lotes)
-                filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", options, format_func=lambda x: 'Nenhum' if x is None else str(x))
-            elif col in ['area_incra', 'area_incra_min', 'area_polig', 'area_polig_min']:
-                options = [None] + sorted(options_area_incra)
-                filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", options, format_func=lambda x: 'Nenhum' if x is None else str(x))
-            elif col == 'data_criac':
-                filters[col] = st.sidebar.date_input(f"Escolha {display_name}:", min_value=pd.to_datetime("1970-01-01"), max_value=pd.to_datetime("2034-12-31"))
-            else:
-                unique_values = [""] + sorted(gdf[col].dropna().unique().tolist())
-                filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", unique_values, format_func=lambda x: 'Nenhum' if x == "" else str(x))
+            if col not in ['uf', 'municipio']:
+                if col in ['lotes', 'quant_fami']:
+                    options = [None] + sorted(options_lotes)
+                    filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", options, format_func=lambda x: 'Todos' if x is None else str(x))
+                elif col in ['area_incra', 'area_incra_min', 'area_polig', 'area_polig_min']:
+                    options = [None] + sorted(options_area_incra)
+                    filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", options, format_func=lambda x: 'Todos' if x is None else str(x))
+                elif col == 'data_criac':
+                    filters[col] = st.sidebar.date_input(f"Escolha {display_name}:", min_value=pd.to_datetime("1970-01-01"), max_value=pd.to_datetime("2034-12-31"))
+                else:
+                    unique_values = [""] + sorted(gdf[col].dropna().unique().tolist())
+                    filters[col] = st.sidebar.selectbox(f"Escolha {display_name}:", unique_values, format_func=lambda x: 'Todos' if x == "" else str(x))
 
     filtered_gdf = gdf.copy()
     for col, value in filters.items():
@@ -127,70 +132,77 @@ if gdf is not None:
                 filtered_gdf = filtered_gdf[filtered_gdf['area_polig'] <= value]
             elif col == 'area_polig_min':
                 filtered_gdf = filtered_gdf[filtered_gdf['area_polig'] >= value]
-            elif col == 'lotes':
-                filtered_gdf = filtered_gdf[filtered_gdf['lotes'] <= value]
-            elif col == 'quant_fami':
-                filtered_gdf = filtered_gdf[filtered_gdf['quant_fami'] <= value]
+            elif col in ['lotes', 'quant_fami']:
+                filtered_gdf = filtered_gdf[filtered_gdf[col] <= value]
             elif col == 'data_criac':
                 filtered_gdf = filtered_gdf[pd.to_datetime(filtered_gdf['data_criac'], errors='coerce') <= pd.to_datetime(value)]
             else:
                 filtered_gdf = filtered_gdf[filtered_gdf[col] == value]
 
-    # Verificar se há resultados filtrados
-    if not filtered_gdf.empty:
-        bounds = filtered_gdf.total_bounds  # retorna (minx, miny, maxx, maxy)
-        m = folium.Map(location=[(bounds[1] + bounds[3]) / 2, (bounds[0] + bounds[2]) / 2], zoom_start=10)
-        m.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
+    # Obter o centro do mapa com base nos filtros
+    if selected_municipality:
+        center = get_center(gdf, municipality=selected_municipality)
+        zoom = 9
+    elif selected_state:
+        center = get_center(gdf, state=selected_state)
+        zoom = 7
     else:
-        st.warning("Nenhum resultado encontrado para os filtros selecionados.")
+        center = [-15.77972, -47.92972]
+        zoom = 4
 
-    for idx, row in filtered_gdf.iterrows():
-        area_formatted = format_area(row.get('area_incra', 0))
-        area_polig_formatted = format_area(row.get('area_polig', 0))
-        tooltip = f"<b>{row.get('nome_pa', 'N/A')} (Assentamento)</b><br>" \
-                  f"Área: {area_formatted} hectares<br>" \
-                  f"Área (segundo polígono): {area_polig_formatted} hectares<br>" \
-                  f"Lotes: {row.get('lotes', 'N/A')}<br>" \
-                  f"Famílias: {row.get('quant_fami', 'N/A')}<br>" \
-                  f"Fase: {row.get('fase', 'N/A')}<br>" \
-                  f"Data de criação: {row.get('data_criac', 'N/A')}<br>" \
-                  f"Forma de obtenção: {row.get('forma_obte', 'N/A')}<br>" \
-                  f"Data de obtenção: {row.get('data_obten', 'N/A')}"
-        folium.GeoJson(
-            row.geometry,
-            tooltip=tooltip,
-        ).add_to(m)
+    m = folium.Map(location=center, zoom_start=zoom)
+
+    # Desenhar polígonos apenas se um estado ou município for selecionado
+    if selected_state or selected_municipality:
+        for _, row in filtered_gdf.iterrows():
+            area_formatted = format_area(row.get('area_incra', 0))
+            area_polig_formatted = format_area(row.get('area_polig', 0))
+            tooltip = f"<b>{row.get('nome_pa', 'N/A')} (Assentamento)</b><br>" \
+                      f"Área: {area_formatted} hectares<br>" \
+                      f"Área (segundo polígono): {area_polig_formatted} hectares<br>" \
+                      f"Lotes: {row.get('lotes', 'N/A')}<br>" \
+                      f"Famílias: {row.get('quant_fami', 'N/A')}<br>" \
+                      f"Fase: {row.get('fase', 'N/A')}<br>" \
+                      f"Data de criação: {row.get('data_criac', 'N/A')}<br>" \
+                      f"Forma de obtenção: {row.get('forma_obte', 'N/A')}<br>" \
+                      f"Data de obtenção: {row.get('data_obten', 'N/A')}"
+            folium.GeoJson(
+                row.geometry,
+                tooltip=tooltip,
+            ).add_to(m)
 
     folium_static(m)
 
     # Baixar polígonos selecionados como GeoJSON
-    geojson = download_geojson(filtered_gdf)
+    if selected_state or selected_municipality:
+        geojson = download_geojson(filtered_gdf)
 
-    st.markdown(f"### Baixar polígonos selecionados como GeoJSON")
-    st.markdown("Clique abaixo para baixar um arquivo GeoJSON contendo os polígonos dos assentamentos selecionados.")
+        st.markdown(f"### Baixar polígonos selecionados como GeoJSON")
+        st.markdown("Clique abaixo para baixar um arquivo GeoJSON contendo os polígonos dos assentamentos selecionados.")
 
-    st.download_button(
-        label="Baixar GeoJSON dos polígonos selecionados",
-        data=geojson,
-        file_name='poligonos_selecionados.geojson',
-        mime='application/json',
-    )
+        st.download_button(
+            label="Baixar GeoJSON dos polígonos selecionados",
+            data=geojson,
+            file_name='poligonos_selecionados.geojson',
+            mime='application/json',
+        )
 
     # Exibir tabela de dados filtrados
-    filtered_gdf = filtered_gdf[['uf', 'municipio', 'cd_sipra', 'nome_pa', 'lotes', 'quant_fami', 'fase', 'area_incra', 'area_polig', 'data_criac', 'forma_obte', 'data_obten']]
-    st.write("Tabela de dados:")
-    st.dataframe(filtered_gdf)
+    if selected_state or selected_municipality:
+        filtered_gdf = filtered_gdf[['uf', 'municipio', 'cd_sipra', 'nome_pa', 'lotes', 'quant_fami', 'fase', 'area_incra', 'area_polig', 'data_criac', 'forma_obte', 'data_obten']]
+        st.write("Tabela de dados:")
+        st.dataframe(filtered_gdf)
 
-    # Baixar dados filtrados como CSV
-    @st.cache(suppress_st_warning=True)
-    def convert_df(df):
-        return df.to_csv(index=False).encode('utf-8')
+        # Baixar dados filtrados como CSV
+        @st.cache_data  # Atualizado para usar st.cache_data
+        def convert_df(df):
+            return df.to_csv(index=False).encode('utf-8')
 
-    csv = convert_df(filtered_gdf)
+        csv = convert_df(filtered_gdf)
 
-    st.download_button(
-        label="Baixar dados filtrados como CSV",
-        data=csv,
-        file_name='dados_filtrados.csv',
-        mime='text/csv',
-    )
+        st.download_button(
+            label="Baixar dados filtrados como CSV",
+            data=csv,
+            file_name='dados_filtrados.csv',
+            mime='text/csv',
+        )
